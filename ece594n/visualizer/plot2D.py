97fa5f74aac3ab9c479f4c7983a73970f5bc8d57
xwang38438 <xin874@ucsb.edu>
2023-03-11 01:46:16 -0800

    modify plots and add explanation

--- ece594n/visualizer/plot2D.py
+++ ece594n/visualizer/plot2D.py
@@ -4,24 +4,58 @@ import plotly.graph_objects as go
 import numpy as np
 
 
-def plot_2d(component1:np.ndarray, component2:np.ndarray,  path:str, y = None,) -> None:
-    
-    fig = go.Figure(data=go.Scatter(
-        x = component1,
-        y = component2,
-        mode='markers',
-        marker=dict(
-            size=20,
-            color=y, #set color equal to a variable
-            colorscale='Rainbow', # one of plotly colorscales
-            showscale=True,
-            line_width=1
+def plot_2d(component1: np.ndarray, 
+            component2: np.ndarray, 
+            path: str, y=None,
+            title: str = None) -> None:
+    # Create a list of unique categories
+    categories = np.unique(y)
+
+    # Create a list of colors for each category
+
+
+    # Create a trace for each category
+    traces = []
+    for i, category in enumerate(categories):
+        mask = y == category
+        trace = go.Scatter(
+            x=component1[mask],
+            y=component2[mask],
+            mode='markers',
+            name=str(category),
+            marker=dict(
+                size=5,
+                colorscale='Rainbow',
+                opacity=0.7,
+                line=dict(width=1)
+            )
         )
-    ))
-    fig.update_layout(margin=dict(l=100,r=100,b=100,t=100),width=2000,height=1200)                 
+        traces.append(trace)
+
+    # Create the figure and add the traces
+    fig = go.Figure(data=traces)
+
+    # Set the layout
+    fig.update_layout(
+        margin=dict(l=100, r=100, b=100, t=100),
+        width=800,
+        height=600,
+        autosize=True,
+        showlegend=True,
+        legend=dict(
+            orientation="h",
+            yanchor="top",
+            y=1.1,
+            xanchor="center",
+            x=0.5
+        ),
+        title = title 
+    )
     fig.layout.template = 'plotly_dark'
-    
+
+    # Show the plot
     fig.show()
+
     
     
     #fig.write_image(path)
--- ece594n/visualizer/plot3D.py
+++ ece594n/visualizer/plot3D.py
@@ -1,34 +1,6 @@
-# For plotting
 import plotly.io as plt_io
 import plotly.graph_objects as go
 import numpy as np
-
-
-# def plot_3d(component1: np.ndarray,
-#             component2 : np.ndarray,
-#             component3 :np.ndarray,
-#             path:str,
-#             y = None) -> None:
-    
-#     fig = go.Figure(data=[go.Scatter3d(
-#             x=component1,
-#             y=component2,
-#             z=component3,
-#             mode='markers',
-#             marker=dict(
-#                 size=10,
-#                 color=y,                # set color to an array/list of desired values
-#                 colorscale='Rainbow',   # choose a colorscale
-#                 opacity=1,
-#                 line_width=1
-#             )
-#         )])
-#     # tight layout
-#     fig.update_layout(margin=dict(l=50,r=50,b=50,t=50),width=1800,height=1000)
-#     fig.layout.template = 'plotly_dark'
-
-#     fig.show()
-#     #fig.write_image(path)
 
 
 
@@ -36,23 +8,62 @@ def plot_3d(component1: np.ndarray,
             component2 : np.ndarray,
             component3 :np.ndarray,
             path:str,
-            y = None) -> None:
+            y = None,
+            title: str = None) -> None:
     
-    fig = go.Figure(data=[go.Scatter3d(
-            x=component1,
-            y=component2,
-            z=component3,
+    # create a list of unique categories in y
+    unique_categories = np.unique(y)
+
+    # create a list of traces for each category
+    traces = []
+    for i, category in enumerate(unique_categories):
+        trace = go.Scatter3d(
+            x=component1[y==category],
+            y=component2[y==category],
+            z=component3[y==category],
             mode='markers',
+            name=str(category),
             marker=dict(
-                size=10,
-                color=y,                # set color to an array/list of desired values
-                colorscale='Rainbow',   # choose a colorscale
+                size=3,
+                color=y[y==category],
+                colorscale='Rainbow',
+                cmin=0,
+                cmax=len(unique_categories) - 1,
+                colorbar=dict(
+                    title="Category",
+                    titleside="right",
+                    tickmode="array",
+                    tickvals=[i],
+                    ticktext=[category]
+                ),
                 opacity=1,
                 line_width=1
             )
-        )])
-    # tight layout with smaller size
-    fig.update_layout(margin=dict(l=50,r=50,b=50,t=50), width=800, height=600, autosize=True)
+        )
+        traces.append(trace)
+
+    # create the plot with multiple traces
+    fig = go.Figure(data=traces)
+
+    # set the layout with legend
+    fig.update_layout(
+        margin=dict(l=50,r=50,b=50,t=50), 
+        width=800, 
+        height=600, 
+        autosize=True,
+        showlegend=True,
+        legend=dict(
+            orientation="h", 
+            yanchor="top", 
+            y=0, 
+            xanchor="center", 
+            x=0.5
+        ),
+        title = title 
+    )
+
+    # set the template
     fig.layout.template = 'plotly_dark'
 
+    # show the plot
     fig.show()
